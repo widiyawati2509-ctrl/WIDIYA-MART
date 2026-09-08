4cb9b3ec081afc908d0e9ac7f5d96016b41d8298
widiyawati2509-ctrl <widiyawati2509@gmail.com>
2026-09-08 17:18:48 +0800

    fix(homepage): restore horizontal slide on product card rows by removing conflicting touch-pan-y

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -159,7 +159,7 @@ export default async function HomePage() {
                 </Link>
               }
             >
-              <div className="flex gap-3 overflow-x-auto scrollbar-hide snap-x snap-mandatory py-2 -mx-4 px-4 touch-pan-y overscroll-x-contain">
+              <div className="flex gap-3 overflow-x-auto scrollbar-hide snap-x snap-mandatory py-2 -mx-4 px-4 overscroll-x-contain">
                 {newestProducts.map((product) => (
                   <div key={product.id} className="w-40 shrink-0 snap-start">
                     <ProductCard product={product} />
@@ -184,7 +184,7 @@ export default async function HomePage() {
                   </Link>
                 }
               >
-                <div className="flex gap-3 overflow-x-auto scrollbar-hide snap-x snap-mandatory py-2 -mx-4 px-4 touch-pan-y overscroll-x-contain">
+                <div className="flex gap-3 overflow-x-auto scrollbar-hide snap-x snap-mandatory py-2 -mx-4 px-4 overscroll-x-contain">
                   {popularProducts.map((product) => (
                     <div key={`popular-${product.id}`} className="w-40 shrink-0 snap-start">
                       <ProductCard product={product} />
@@ -210,7 +210,7 @@ export default async function HomePage() {
                   </Link>
                 }
               >
-                <div className="flex gap-3 overflow-x-auto scrollbar-hide snap-x snap-mandatory py-2 -mx-4 px-4 touch-pan-y overscroll-x-contain">
+                <div className="flex gap-3 overflow-x-auto scrollbar-hide snap-x snap-mandatory py-2 -mx-4 px-4 overscroll-x-contain">
                   {cat.products.map((product) => (
                     <div key={`${cat.slug}-${product.id}`} className="w-40 shrink-0 snap-start">
                       <ProductCard product={product} />
